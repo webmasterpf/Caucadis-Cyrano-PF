25682d4007300eedb2d059ea2ecda0ac62060eca
webmasterpf-DG <webmaster@provence-formation.com>
2011-05-19 17:26:02 +0200

    Changement style de sortie pour liste VDL global
    Signed-off-by: webmasterpf-DG <webmaster@provence-formation.com>

--- includes/inc_vue_vdl_globale_sortie.php
+++ includes/inc_vue_vdl_globale_sortie.php
@@ -24,3 +24,23 @@ if ($view->result) {
 print $output;
 
 ?>
+
+<?php
+$viewname = 'vdl_global';
+$view = views_get_view ($viewname);
+$view->set_display('block_2');
+
+
+//Exécution de le vue
+$view->pre_execute();
+$view->execute();
+
+if ($view->result) {
+  // S'il y a un resultat on récupère le titre (ajoute tag h3, et le contenu)
+  $output = '<div id="bloc_vdl_sortie"><p>Style ligne</p><h3>'.$view->get_title().'</h3>' . $view->render().'</div>';
+}
+
+//Affiche la vue
+print $output;
+
+?>
