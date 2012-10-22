2239cfd617ed6ee81e6a5489d6096fb48e10e4af
webmasterpf-DG <webmaster@provence-formation.com>
2012-10-22 17:06:47 +0200

    Ajout quantcast
    Signed-off-by: webmasterpf-DG <webmaster@provence-formation.com>

--- js/code_stats.php
+++ js/code_stats.php
@@ -46,16 +46,27 @@ woopraTracker.setDomain("lycee-caucadis.com");
 woopraTracker.track();
 </script>
 <!-- Woopra Code End -->
-
-
-<!-- Start Quantcast tag -->
+<!-- Quantcast Tag -->
 <script type="text/javascript">
-_qoptions={
+var _qevents = _qevents || [];
+
+(function() {
+var elem = document.createElement('script');
+elem.src = (document.location.protocol == "https:" ? "https://secure" : "http://edge") + ".quantserve.com/quant.js";
+elem.async = true;
+elem.type = "text/javascript";
+var scpt = document.getElementsByTagName('script')[0];
+scpt.parentNode.insertBefore(elem, scpt);
+})();
+
+_qevents.push({
 qacct:"p-behZUd_cXqsz-"
-};
+});
 </script>
-<script type="text/javascript" src="http://edge.quantserve.com/quant.js"></script>
+
 <noscript>
-<img src="http://pixel.quantserve.com/pixel/p-behZUd_cXqsz-.gif" style="display: none;" border="0" height="1" width="1" alt="Quantcast"/>
+<div style="display:none;">
+<img src="//pixel.quantserve.com/pixel/p-behZUd_cXqsz-.gif" border="0" height="1" width="1" alt="Quantcast"/>
+</div>
 </noscript>
 <!-- End Quantcast tag -->
